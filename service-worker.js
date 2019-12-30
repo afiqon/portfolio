3561e28f3906478a3ed69abd914b400b2a542b22
Afiq <afiqon@gmail.com>
2019-12-30 23:28:27 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.aab67eba0a47e06a96067d31bb9dec7d.js"
+  "/portfolio/precache-manifest.55cb667ef9bd5f3a15961d0b67a62f07.js"
 );
 
 self.addEventListener('message', (event) => {
